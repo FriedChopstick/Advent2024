bc61fc52d98a602ebe9162db41c47b3d7f17e50e
Ryan Liu <ryanliu12620@gmail.com>
2024-12-10 23:17:43 -0500

    Got 755 openings for mul(, correctly as according to ctrl f and put into list. Gonna go hit the sack

--- src/AdventDay3.py
+++ src/AdventDay3.py
@@ -1,7 +1,31 @@
-with open("AdventDay3Input.txt") as Advent_Input:
-    Advent_Input_Value =  Advent_Input.read()
+from collections import Counter
+
+
+with open("src/AdventDay3Input.txt") as Advent_Input:
+    Advent_Input_Value = Advent_Input.read()
     print(Advent_Input_Value)
 
-Advent_Input_Value = Advent_Input_Value.replace("mul", "#")
-print(Advent_Input_Value)
 
+opener = "mul("
+closer = ")"
+all_muls = []
+
+while Advent_Input_Value.find(opener) != -1:
+    new_input = Advent_Input_Value[Advent_Input_Value.find(opener) :]
+    print(new_input)
+    all_muls.append(new_input[new_input.find(opener) : new_input.find(closer) + 1])
+    Advent_Input_Value = Advent_Input_Value[Advent_Input_Value.find(opener) + 2 :]
+
+
+print(all_muls)
+print(len(all_muls))
+
+# for i in range(1,10):
+#     Advent_Input_Value = Advent_Input_Value.replace(str(i), "")
+
+
+# print(Advent_Input_Value)
+# print(Advent_Input_Value.count("mul(,)"))
+
+
+# Investigate regular expressions
