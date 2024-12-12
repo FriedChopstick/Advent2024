007dc4a95c1fec4065a2a5d35f6e1e3045b4c108
Ryan Liu <ryanliu12620@gmail.com>
2024-12-11 23:07:19 -0500

    Ima do part two now

--- src/AdventDay3.py
+++ src/AdventDay3.py
@@ -25,6 +25,8 @@ closer = ")"
 all_muls = []
 total = 0
 
+### Part 1
+
 # Goes through input and appends any possible mults to a list
 while Advent_Input_Value.find(opener) != -1:
     new_input = Advent_Input_Value[Advent_Input_Value.find(opener) :]
@@ -36,8 +38,34 @@ for i, mul in enumerate(all_muls):
     if check_if_valid(mul):
         total += calculate_mul(mul)
 
-# Part 1
 print(f"Part 1 Total: {total}")
 
+# Part 2 
+all_muls = []
+total = 0
+to_do = "do()"
+to_not_do = "don't()"
+to_do_bool = True
 
+with open("src/AdventDay3Input.txt") as Advent_Input:
+    Advent_Input_Value = Advent_Input.read()
+
+while Advent_Input_Value.find(opener) != -1:
+    to_do_bool = Advent_Input_Value.find(to_do) < Advent_Input_Value.find(to_not_do)
+    if to_do_bool:
+        new_input = Advent_Input_Value[Advent_Input_Value.find(opener) :]
+        all_muls.append(new_input[new_input.find(opener) : new_input.find(closer) + 1])
+        Advent_Input_Value = Advent_Input_Value[Advent_Input_Value.find(opener) + 2 :]
+    else:
+        new_input = Advent_Input_Value[Advent_Input_Value.find(opener) :]
+        Advent_Input_Value = Advent_Input_Value[Advent_Input_Value.find(opener) + 2 :]
+
+print(len(all_muls))
+print(all_muls)
+
+for i, mul in enumerate(all_muls):
+    if check_if_valid(mul):
+        total += calculate_mul(mul)
+
+print(f"Part 2 Total: {total}")
 # Investigate regular expressions
